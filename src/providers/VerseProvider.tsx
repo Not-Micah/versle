cffcd3b49ca462bc64cdce3433b2d84eb0065e0d
Micah Tidball <micahtid@gmail.com>
2025-01-31 14:33:29 +0700

    updated API fetch link!

--- src/providers/VerseProvider.tsx
+++ src/providers/VerseProvider.tsx
@@ -18,7 +18,7 @@ const getDailyVerse = async () => {
   try {
     //http://localhost:3000/api/verses
     //https://versle.vercel.app/api/verses
-    const res = await fetch("http://localhost:3000/api/verses", {
+    const res = await fetch("https://versle.vercel.app/api/verses", {
       cache: "no-store",
     });
 
